a46095967eadcfa4b95b52d4d1ffd33276d237d2
Amila29 <amilakasun2000@gmail.com>
2024-06-03 21:57:55 +0530

    Updated home.view to render components conditionally

--- src/screens/home/home.view.js
+++ src/screens/home/home.view.js
@@ -4,22 +4,42 @@ import data from "../../data/data.json";
 import "./home.view.css";
 import Slider from "react-slick";
 import HorizontalSlider from "../../components/slider/slider";
+import { app } from "../../config/firebase";
+import { useState, useEffect } from "react";
+import { onAuthStateChanged, getAuth } from "firebase/auth";
 
 function Home() {
+
+  const [user, setUser] = useState(null);
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+
   return (
     <div>
-      <div className="container">
+      {user ? <div className="container">
         <h1 className="title">Best Matches</h1>
         <HorizontalSlider data={data} />
-      </div>
+      </div> : null}
 
-      <div className="container">
-        <h1 className="title">Best Matches</h1>
+        <br />
+
+      {user ? <div className="container">
+        <h1 className="title">Best Tutor for you</h1>
         <HorizontalSlider data={data} />
-      </div>
+      </div> : null}
+
+        <br />
 
       <div className="container">
-        <h1 className="title">Best Matches</h1>
+        <h1 className="title">Most Popular</h1>
         <HorizontalSlider data={data} />
       </div>
     </div>
