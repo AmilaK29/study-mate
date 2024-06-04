c2588c71baec4fc9fa5778ed6043b30e1f18748c
Amila29 <amilakasun2000@gmail.com>
2024-06-04 16:48:50 +0530

    Added the functionalities Fetching subjects that a student can teach,Getting tutors for a subject

---
+++ src/controller/display.controller.js
@@ -0,0 +1,73 @@
+
+import { db } from "../config/firebase";
+import { getDoc, doc, getDocs, collection } from "firebase/firestore";
+
+export async function getSubujectKnowsArray(email){
+    //This function will return the array of subjects that a
+    //student can tutor.
+    // input : student's email
+
+    try {
+        const docRef = doc(db, "student", email);
+        const querySnapshot = await getDoc(docRef);
+        const subjects = [];
+        if (querySnapshot.exists()) {
+            console.log("Document data:", querySnapshot.data());
+            for (const subject of querySnapshot.data().subjects_student_knows) {
+                subjects.push(subject);
+            }
+            return { status: true, data: subjects };
+        }
+        return { status: true, data: subjects };
+        
+    } catch (error) {
+        console.error("Error during fetching profile details:", error.message);
+        return { status: false ,data : []};
+    }
+
+}
+
+export async function getTutorsForASubject(subject){
+    //This function will return the array of tutors that can teach the subject
+    // input : subject
+    try {
+        const docRef = collection(db, "student");
+        const docSnap = await getDocs(docRef);
+        const tutors = [];
+        if(docSnap.empty){
+            return { status: true, data: tutors };
+        }
+        for (const doc of docSnap.docs) {
+            for (const subject_description of doc.data().subjects_student_knows) {
+                // console.log(subject_description.subject);
+                if(subject_description.subject === subject){
+                    tutors.push(doc.data());
+                }
+            }
+        }
+        return { status: true, data: tutors };
+    } catch (error) {
+        console.error("Error during fetching tutors:", error.message);
+        return { status: false ,data : []};
+    }
+}
+
+export async function getSubjectsNeedsATutor(email){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export async function getTutorsForAStudent(email){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+
+
