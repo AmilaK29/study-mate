7ac8bf14f4f498e6bbcc4889625d7a78d397c776
Amila29 <amilakasun2000@gmail.com>
2024-06-05 20:50:58 +0530

    Added the functionalities in display.controller to home.view

--- src/screens/home/home.view.js
+++ src/screens/home/home.view.js
@@ -7,7 +7,15 @@ import HorizontalSlider from "../../components/slider/slider";
 import { app } from "../../config/firebase";
 import { useState, useEffect } from "react";
 import { onAuthStateChanged, getAuth } from "firebase/auth";
-import { getSubujectKnowsArray ,getTutorsForASubject,getSubjectsNeedsATutor,getTutorsForAStudent} from "../../controller/display.controller";
+import { 
+    getSubujectKnowsArray ,
+    getTutorsForASubject,
+    getSubjectsNeedsATutor,
+    getTutorsForAStudent,
+    getBestMatches
+} from "../../controller/display.controller";
+
+
 
 
 function Home() {
@@ -15,6 +23,7 @@ function Home() {
   const [user, setUser] = useState(null);
   const auth = getAuth(app);
   const [tutorNeedsSubjects, setTutorNeedsSubjects] = useState([]);
+  const [bestMatches, setBestMatches] = useState([]);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -29,18 +38,25 @@ function Home() {
     //   console.log("Subjects",response.data);
     // });
     getTutorsForAStudent('amal2000@gmail.com').then((response) => {
+      console.log(response.data);
       setTutorNeedsSubjects(response.data);
-      console.log("Tutors : " + tutorNeedsSubjects);
+    //   console.log("Tutors : " + tutorNeedsSubjects);
+    });
+
+    getBestMatches('amal2000@gmail.com').then((response) => {
+        console.log(response.data);
+        setBestMatches(response.data);
+        // console.log("Best Matches : " + bestMatches);
     });
   },[]);
 
 
   return (
     <div>
-      {/* {user ? <div className="container">
+      {user && bestMatches.length !==0 ? <div className="container">
         <h1 className="title">Best Matches</h1>
-        <HorizontalSlider data={data} />
-      </div> : null} */}
+        <HorizontalSlider data={bestMatches} />
+      </div> : null}
 
         <br />
 
